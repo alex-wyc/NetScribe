47abe55ebd1705741dda26da4a809cdd9ed2a17d
Yicheng Wang <alex.wyc2098@protonmail.ch>
2016-01-19 22:12:42 -0500

    updated

--- server.h
+++
@@ -1,9 +0,0 @@
-#ifndef CHAT_SERVER_H
-#define CHAT_SERVER_H
-
-typedef struct connection {
-    int id; // id of the connection, also the index of it along the pipes
-    char name[32]; // username of the client
-} connection
-
-#endif
