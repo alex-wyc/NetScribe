15cb9a82d68c768dd8a87ec4b9cc010cc1147f0d
Ruojia Sun <sunruojia@gmail.com>
2016-01-24 12:52:24 -0500

    updated

--- central.h
+++ central.h
@@ -1,13 +1,16 @@
 #ifndef CENTRAL_SERVER_H_
 #define CENTRAL_SERVER_H_
 
+//room and subserver are used interchangeably for now since idk if we're gonna change the struct name
+
 typedef struct client { // a client connection
     char name[16];
-    int sub_serv_id; // which subserver this client is connected to
+    int room_id; // which room this client is in. can we let it correspond to the index in rooms_list?
+    int socket_id; // central server <-> client connection
 } client;
 
-typedef struct subserver { // a subserver connection
-    int pipe[2];
+typedef struct subserver {
+    //int pipe[2];
     client *users;
     int *on_line; // one-to-one correspondence with the users, if the user is online
 } subserver;
---
+++ chat_room.h
@@ -0,0 +1,5 @@
+#include "central.h"
+
+int run_chat_room(char * message, int socket_sender, client * users_list, subserver * rooms_list);
+int distribute_message(char * message, client * sender, subserver * room);
+char * format_message(char * name, char * message);
