8cf835eb6e00f8f2b2c9bd9d4e1124a36030c11d
Yicheng Wang <alex.wyc2098@protonmail.ch>
2016-01-26 11:23:47 -0500

    updated

--- distribute.h
+++ distribute.h
@@ -4,6 +4,6 @@
 #define _DISTRIBUTE_H
 
 message *parse(char *buf); // parses buffer into an incoming message struct
-void distribute(int user_ids[], int sz, message incoming); // sends the message across a list of user ids with size sz
+void distribute(int user_ids[], int sz, client *users_list[], message incoming); // sends the message across a list of user ids with size sz
 
 #endif // _DISTRIBUTE_H
